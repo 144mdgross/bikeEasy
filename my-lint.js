97fc8fba629b2e75f02be538ac139569e70db7f2
144mdgross <144.mdgross@gmail.com>
2017-03-05 19:17:21 -0700

    adding my linting rules.

---
+++ my-lint.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "extends": [
+    'ryansobol/browser',
+    'ryansobol/es6',
+    'ryansobol/jquery'
+  ],
+  rules: {
+    "semi": "off",
+    "prefer-const": "off",
+    "no-implicit-globals": "off",
+    "strict": "off",
+    "quotes": "off",
+    "no-unused-vars": "off",
+    "no-else-return": "off",
+    "no-multiple-empty-lines": "off",
+    "object-shorthand": ["error", "consistent"]
+  }
+}
